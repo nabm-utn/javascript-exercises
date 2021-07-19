53f40f4b77e38325e6f3500f3e6f4cb8ca9167c4
nabm-utn <nabm.utn@gmail.com>
2021-07-19 09:27:40 -0300

    solve removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray) {
+    let outputArray = [];
+    let removed = [];
+    for (let i = 1; i < arguments.length; i++) {
+        removed.push(arguments[i]);
+    }
+    for (let i = 0; i < inputArray.length; i++) {
+        if (removed.includes(inputArray[i])) {
+            continue;
+        } else {
+            outputArray.push(inputArray[i]);
+        }
+    }
+    return outputArray;
 
 };
 
