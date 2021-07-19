64fbf8c0aae617fc26cc838ae0d8dd6ed7c98eeb
nabm-utn <nabm.utn@gmail.com>
2021-07-19 08:59:14 -0300

    solves repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repetitions) {
+    if (repetitions < 0) {
+        return "ERROR";
+    }
+    outputString = "";
+    for (let i = 0; i < repetitions; i++) {
+        outputString += inputString;
+    } 
+    return outputString;
 };
 
 module.exports = repeatString;
