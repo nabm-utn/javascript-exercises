ac92c71c5c433d544fbfa5847d327bcec001a232
nabm-utn <nabm.utn@gmail.com>
2021-07-19 09:38:14 -0300

    solve sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let increment = 1;
+    if (a > b) {increment = -1;}
+    let sum = 0;
+
+    if (typeof a != "number" || typeof b != "number"){
+        return "ERROR";
+    } else if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    for (let i = a; i != b; i += increment) {
+        sum += i;
+    }
+
+    return sum + b;
 
 };
 
