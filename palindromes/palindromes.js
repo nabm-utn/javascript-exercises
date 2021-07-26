246210a2248a33eb8a98b47028f70ea8389a9b6b
nabm-utn <nabm.utn@gmail.com>
2021-07-26 20:24:01 -0300

    solve palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+let straight = Array.from(string.toLowerCase())
+straight = straight.filter(char => /^[a-z]+$/.test(char))
+reverse = [...straight].reverse();
+for (let i = 0; i < straight.length; i++) {
+    if (straight[i] !== reverse[i]) return false;
+} return true;
+}; 
 
 module.exports = palindromes;
