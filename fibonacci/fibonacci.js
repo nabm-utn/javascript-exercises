36327aeaab6f834c4e20b97297b3e73702c5d85d
nabm-utn <nabm.utn@gmail.com>
2021-07-26 20:29:15 -0300

    solve fibbonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    a = parseInt(a);
+    if (a < 0) return "OOPS";
+    if (a === 0) return 0;
+    if (a === 1) return 1;
+    if (a === 2) return 1;
+    return fibonacci(a-1) + fibonacci(a-2);
 };
 
 module.exports = fibonacci;
