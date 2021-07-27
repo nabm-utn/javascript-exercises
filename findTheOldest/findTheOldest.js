f1eef51784a9659b2f538b616f6193b6dae62f17
nabm-utn <nabm.utn@gmail.com>
2021-07-26 22:09:06 -0300

    solve findTheOldest.js

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let sortedArray = array.sort((a, b) => {
+        const birthA = a.yearOfBirth;
+        let deathA = a.yearOfDeath;
+        if (!deathA) deathA = new Date().getFullYear();
+        const ageA = deathA - birthA;
 
+        const birthB = b.yearOfBirth;
+        let deathB = b.yearOfDeath;
+        if (!deathB) deathB = new Date().getFullYear();
+        const ageB = deathB - birthB;
+
+        return ageA < ageB ? 1: -1;
+    })
+    return sortedArray[0];
 };
 
 module.exports = findTheOldest;
